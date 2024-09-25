8bf52c2106a22323d93a8cba9fd35a738838ed2d
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2024-09-25 14:36:51 +0900

    Update hello_server_win.cpp

--- TCP_IP_Study/hello_server_win.cpp
+++ TCP_IP_Study/hello_server_win.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <WinSock2.h>
 
-int main(void) {
-	std::cout << "hello world " << std::endl;
+using namespace std;
+void ErrorHandling(char* message);
+
+int main(int argc, char* argv[]) {
+	WSADATA wsaData;
+	SOCKET hServerSock, hClntSock;
+	SOCKADDR_IN servAddr, clntAddr;
+
+	int szClntAddr;
+	char message[] = "Hello World!";
+	if (argc != 2) {
+		cout << "Usage:" << argv[0] << "<port>" << endl;
+		exit(1);
+	}
+
+	return 0;
 }
