fdbdf10e531c78bc2b524052da6f50942a5d8390
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2024-09-25 15:22:23 +0900

    38번줄

--- TCP_IP_Study/hello_server_win.cpp
+++ TCP_IP_Study/hello_server_win.cpp
@@ -2,7 +2,7 @@
 #include <WinSock2.h>
 
 using namespace std;
-void ErrorHandling(char* message);
+void ErrorHandling(const char* message);
 
 int main(int argc, char* argv[]) {
 	WSADATA wsaData;
@@ -15,6 +15,31 @@ int main(int argc, char* argv[]) {
 		cout << "Usage:" << argv[0] << "<port>" << endl;
 		exit(1);
 	}
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+		ErrorHandling("WSAStartupError");
+	}
+	hServerSock = socket(PF_INET, SOCK_STREAM, 0);
+	if (hServerSock == INVALID_SOCKET) {
+		ErrorHandling("socket() error");
+	}
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);//S_un.s_addr = s_addr
+	servAddr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(hServerSock, reinterpret_cast<sockaddr*>(&servAddr), sizeof(servAddr) == SOCKET_ERROR) ){
+		ErrorHandling("bind() error");
+	}
+
+	if (listen(hServerSock, 5) == INVALID_SOCKET) {
+		ErrorHandling("listen() error");
+	}
+	szClntAddr =sizeof(clntAddr);
 
 	return 0;
+}
+
+void ErrorHandling(const char* message) {
+	fputs(message, stderr);
+	fputc('\n', stderr);
 }
